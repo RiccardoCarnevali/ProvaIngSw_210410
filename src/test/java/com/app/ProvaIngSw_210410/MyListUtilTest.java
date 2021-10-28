b16ce3e7419b2119a42d13e7acb1c926261e4a44
unknown <riccardo.carnevali@gmail.com>
2021-10-28 09:24:10 +0200

    Aggiunti dei messaggi per indicare l'orario di inizio e fine dei test

--- src/test/java/com/app/ProvaIngSw_210410/MyListUtilTest.java
+++ src/test/java/com/app/ProvaIngSw_210410/MyListUtilTest.java
@@ -4,10 +4,35 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 
+import org.joda.time.DateTime;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MyListUtilTest {
+	
+	@BeforeClass
+	public static void startAllTests() {
+		System.out.println("Orario di inizio di tutti i test: " + new DateTime().toString());
+	}
 
+	@AfterClass
+	public static void endAllTests() {
+		System.out.println("Orario di fine di tutti i test: " + new DateTime().toString());
+	}
+	
+	@Before
+	public void startSingleTest() {
+		System.out.println("Orario di inizio di un test: " + new DateTime().toString());
+	}
+	
+	@After
+	public void endSingleTest() {
+		System.out.println("Orario di fine di un test: " + new DateTime().toString());
+	}
+	
 	@Test
 	public void ascendingOrderingWorks() {
 		assertEquals(Arrays.asList(new Integer[] {-14, -9, 3, 7, 10, 14, 20, 567}), new MyListUtil().sort(Arrays.asList(new Integer[] {20, -9, 3, 14, 567, 7, -14, 10}), 0));
